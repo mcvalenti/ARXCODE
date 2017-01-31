4ef947a2288bfd2d820fbe03772c71d0d2b64876
mcvalenti <mcvalenti717@hotmail.com>
2017-01-31 17:08:41 -0300

    conexion a NORAD

--- TleAdmin/get_tle.py
+++ TleAdmin/get_tle.py
@@ -5,34 +5,34 @@ Created on Tue Jul 12 14:21:56 2016
 """
 
 from requests import session
+from datetime import datetime
 
 class importar_tle():
     
-    def __init__(self, cat_id,tca):
+    def __init__(self, cat_id,tca0,tca1):
         self.f=open(cat_id+'.txt','w')
         self.noradId=cat_id
-        self.epoca=tca
-#         y=tca.year
-#         m=tca.month
-#         di=tca.day
-#         df=di+2
-#         fecha=str(y)+'-'+'0'+str(m)+'-'+'0'+str(di)
-#         fecha1=str(y)+'-'+'0'+str(m)+'-'+str(df) 
-#         
-#     def ConsultaAutentica(self):
-#         print 'Ingrese su Usuario: '
-#         usuario=raw_input()
-#         print 'Ingrese su Clave' # MaCeciliaSpace17
-#         clave= raw_input()
-#         data = {'identity': usuario , 'password': clave}
-#         return data
-#     
-#     def Retrieve(self):
-#         data = ConsultaAutentica()
-#         s=session()
-#              
-#         s.post('https://www.space-track.org/auth/login',data)
-#         fquery1 = "https://www.space-track.org/basicspacedata/query/class/tle/EPOCH/2013-03-01/NORAD_CAT_ID/37673/orderby/TLE_LINE1 ASC/format/tle"
-#         r = s.get(fquery1)
-#         f.write(r.text)
-#         print 'Se ha generado el archivo= ', cat_id,'.txt'
+        self.epoca=tca0
+        self.fini=datetime.strftime(tca0,'%Y-%m-%d')
+        self.ffin=datetime.strftime(tca1,'%Y-%m-%d')
+        self.tleDato=self.solicitudTle()
+        self.f.write(self.tleDato)
+        print 'Se ha generado el archivo= ', cat_id,'.txt'
+
+    def ConsultaAutentica(self):
+        print 'Ingrese su Usuario: '
+        usuario=raw_input()
+        print 'Ingrese su Clave' # MaCeciliaSpace17
+        clave= raw_input()
+        data = {'identity': usuario , 'password': clave}
+        return data
+    
+    def solicitudTle(self):
+        data = self.ConsultaAutentica()
+        s=session() 
+        s.post('https://www.space-track.org/auth/login',data)
+        f1='2013-03-01'
+        f2='2013-03-03'
+        fquery1='https://www.space-track.org/basicspacedata/query/class/tle/EPOCH/'+f1+'--'+f2+'/NORAD_CAT_ID/37673/orderby/TLE_LINE1 ASC/format/tle'
+        r = s.get(fquery1)
+        return r.text
--- pruebas/request.py
+++ pruebas/request.py
@@ -6,5 +6,6 @@ Created on 30/01/2017
 from datetime import datetime
 from TleAdmin.get_tle import importar_tle
 
-tca=datetime(2003,03,01,00,00,00)
-importar_tle('37673',tca)
+tca0=datetime(2003,03,01)
+tca1=datetime(2003,03,03)
+importar_tle('37673',tca0,tca1)
